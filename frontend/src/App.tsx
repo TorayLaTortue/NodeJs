7faabec21ddb8f02f7af11f12576341b4139baf2
Toray <damien.pasquer3636@gmail.com>
2024-05-06 09:27:49 +0200

    #

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
 import { BrowserRouter, Link } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
 import Routing from './routes/Routing';
+import store from './app/store';
 import React from 'react';
+import { Provider } from 'react-redux';
 
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Provider store={store}>
+      <div className="App">
+        </div>
+    </Provider>
       <CssBaseline />
       <Routing />
     </BrowserRouter>
--- frontend/src/pages/Auth/Register.tsx
+++ frontend/src/pages/Auth/Register.tsx
@@ -1,30 +1,7 @@
-import React, { useState, FormEvent } from 'react';
+import React from 'react';
 
-interface Owner {
-  firstName: string;
-}
-
-const Register: React.FC = () => {
-  const [owner, setOwner] = useState<Owner | null>(null);
-
-  const handleSubmit = (event: FormEvent<HTMLFormElement>): void => {
-    event.preventDefault();
-    const firstName = (event.target as HTMLFormElement).firstName.value;
-    setOwner({ firstName });
-  };
-
-  return (
-    <div>
-      <h2>Register</h2>
-      <div id="header">
-        {owner ? `Le propriétaire du restaurant est ${owner.firstName}` : 'Aucun propriétaire configuré'}
-      </div>
-      <form onSubmit={handleSubmit}>
-        <input name="firstName" />
-        <button type="submit">Enregistrer</button>
-      </form>
-    </div>
-  );
+const Register = () => {
+  return <div>Register</div>;
 };
 
 export default Register;
